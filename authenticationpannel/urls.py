cea761c85a7cb399b255b3d4b9c593e1f494e881
anuragdeFUAS <anurag.de@stud.fra-uas.de>
2024-01-12 22:23:08 +0100

    changes made to incorporate media and static urls

--- authenticationpannel/urls.py
+++ authenticationpannel/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # Django admin
     path("", include(("app.urls", "app"), namespace="app")),
     path("api/", include(("api.urls", "api"), namespace="api")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
