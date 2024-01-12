0a0953ecdd703b2b0c181dac5da811197213c576
anuragdeFUAS <anurag.de@stud.fra-uas.de>
2024-01-12 18:53:39 +0100

    Revert "final tests for static file problem"
    This reverts commit 72daea636707af5e801a4e8b429e9de17ad13629.

--- authenticationpannel/settings.py
+++ authenticationpannel/settings.py
@@ -47,8 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",    
-    'whitenoise.runserver_nostatic',
+    "django.contrib.staticfiles",
 ]
 
 REST_FRAMEWORK = {
@@ -66,7 +65,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "authenticationpannel.urls"
@@ -132,10 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
